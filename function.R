bd01950797bf710f7fc7b88218087a1fd27a1c4d
ckameni <e.c.kameni@aim.com>
2014-12-22 01:16:30 +0100

    done: Project function.r

--- function.R
+++ function.R
@@ -1,21 +1,33 @@
-#Submission
-# predict outcome levels on the original Testing data set using Random Forest algorithm
-predictfinal <- predict(model2, testDf, type="class")
-predictfinal
-##  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 
-##  B  A  B  A  A  E  D  B  A  A  B  C  B  A  E  E  A  B  B  B 
-## Levels: A B C D E
-# Write files for submission
 
-txtFiles = function(x){
-      n = length(x)
-      for(i in 1:n){
-            title = paste0("Submission_",i,".txt")
-            write.table(x[i],file=title,quote=FALSE,row.names=FALSE,col.names=FALSE)
-      }
-}
+library(caret,quietly=TRUE)
+library(randomForest,quietly=TRUE) 
 
-txtFiles(endPrediction)
+trainingDf<-read.csv("./pml-training.csv", na.strings=c("", "NA", "NULL"))
+dim(trainingDf)# Check number of variables and observations
+
+testDf<-read.csv("./pml-testing.csv", na.strings=c("", "NA", "NULL"))
+dim(testDf)# Check number of variables and observations
+
+trainingDf<-trainingDf[,colSums(is.na(trainingDf)) == 0];dim(trainingDf)
+testDf <-testDf[,colSums(is.na(testDf)) == 0];dim(testDf)
+
+trainingDf <-trainingDf[,-c(1:7)];dim(trainingDf)
+testDf <-testDf[,-c(1:7)];dim(testDf)
+
+set.seed(1442)
+data <- createDataPartition(y=trainingDf$classe, p=0.70, list=FALSE)
+train.Sub.trainingDf <- trainingDf[data, ];dim(train.Sub.trainingDf)
+test.Sub.trainingDf<- trainingDf[-data, ];dim(test.Sub.trainingDf)
 
 
+model <- randomForest(classe ~. , data=train.Sub.trainingDf, method="class")
 
+# perform the Prediction
+prediction <- predict(model, test.Sub.trainingDf, type = "class")
+
+# Test the prediction:
+confusionMatrix(prediction, test.Sub.trainingDf$classe)
+
+
+endPrediction <- predict(model, testDf, type="class")
+endPrediction
